93d7761209b001e575971ea7dcf6cb78b47cd8ce
Chris <chris.goringe@gmail.com>
2023-12-20 10:19:24 +1100

    better nudge

--- nudge_conditioning_node.py
+++ nudge_conditioning_node.py
@@ -6,7 +6,8 @@ class NudgeConditioning:
     def INPUT_TYPES(s):
         return {"required": {   "conditioning": ("CONDITIONING", ), 
                                 "nudge_file": ("STRING", {"default":""}),
-                                "nudge_strength": ("FLOAT", {"default": 0.1, "step": 0.01}),
+                                "nudge_strength": ("FLOAT", {"default": 1.0, "step": 0.01}),
+                                "method": (["77","nudge"],),
                              }}
     
     RETURN_TYPES = ("CONDITIONING",)
@@ -14,12 +15,11 @@ class NudgeConditioning:
 
     CATEGORY = "conditioning"
 
-    def func(self, conditioning, nudge_file:str, nudge_strength:float):
+    def func(self, conditioning, nudge_file:str, nudge_strength:float, method:str):
         # T = tokens = 77
         # L = length = 2048
         # B = Batch
         # F = Features = 768
-        method1=False
         c1:torch.Tensor = load_file(nudge_file)['nudge']
         F = c1.shape[0]
         c1 = c1.reshape((1,1,F)) 
@@ -29,7 +29,12 @@ class NudgeConditioning:
         
         c0:torch.Tensor = conditioning[0][0]                            
         B, T, L = c0.shape
-        c1 = torch.cat( [c1.repeat((B,T,1))] + [torch.zeros( (B,T,(L-F)) )], dim=2)
-        return( [[c1 + c0,conditioning[0][1].copy()],], )
+        if method=="nudge":
+            c1 = torch.cat( [c1.repeat((B,T,1))] + [torch.zeros( (B,T,(L-F)) )], dim=2)
+            return( [[c1 + c0,conditioning[0][1].copy()],], )
+        elif method=="77":
+            c1 = torch.cat( [c1] + [torch.zeros( (1,1,(L-F)) )], dim=2)
+            c0[:,-1,:] = c1
+            return( [[c0,conditioning[0][1].copy()],], )
 
 
