bdaf57c04540ad761e396247e9932a07c91f58b7
Chris <chris.goringe@gmail.com>
2023-12-22 08:49:16 +1100

    nudges

--- nudge_conditioning_node.py
+++ nudge_conditioning_node.py
@@ -1,31 +1,65 @@
 from safetensors.torch import load_file
 import torch
 
-class NudgeConditioning:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required": {   "conditioning": ("CONDITIONING", ), 
-                                "nudge_file": ("STRING", {"default":""}),
-                                "nudge_strength": ("FLOAT", {"default": 1.0, "step": 0.01}),
-                                "replace_token": ("INT",{"default":76, "min":0, "max":76}),
-                             }}
-    
+class BaseNudge:
     RETURN_TYPES = ("CONDITIONING",)
     FUNCTION = "func"
-
     CATEGORY = "conditioning"
 
-    def func(self, conditioning, nudge_file:str, nudge_strength:float, replace_token:int):
-        if len(conditioning)>1: print("Warning - conditionings after the first are being lost")
-        
-        c0:torch.Tensor = conditioning[0][0].clone()
-        L = c0.shape[2]
-        c1:torch.Tensor = load_file(nudge_file)['nudge']
-        F = c1.shape[0]
-        if (L<F): c1 = torch.cat( [c1] + [torch.zeros((L-F)) ])
-        replaced = c0[:,replace_token,:]
- 
-        c0[:,replace_token,:] = torch.mul(c1,nudge_strength) + torch.mul(replaced,1-nudge_strength)
-        return( [[c0,conditioning[0][1].copy()],], )
+    REQUIRED = {}
 
+    STD = {  "conditioning": ("CONDITIONING", ), 
+                    "token_strength": ("FLOAT", {"default": 1.0, "step": 0.01}),
+                    "replace_token": ("INT",{"default":76, "min":-1, "max":76}),
+                    "pool_strength": ("FLOAT", {"default": 1.0, "step": 0.01}) }
 
+    @classmethod
+    def ADD_STANDARD_INPUT_TYPES(cls, it):
+        for k in cls.STD:
+            it['required'][k] = cls.STD[k]
+        for k in cls.REQUIRED: it['required'][k] = cls.REQUIRED[k]
+        return it
+
+    def _func(self, conditioning_in:list, conditioning_nudge:torch.Tensor, token_strength:float, replace_token:int, pool_strength:float):
+        if len(conditioning_in)>1: print("Warning - only the first conditioning is nudged")
+        c0:torch.Tensor = conditioning_in[0][0].clone()
+        dic = conditioning_in[0][1].copy()
+        assert conditioning_nudge.shape[0]==c0.shape[2], "Nudge incompatible with conditioning"
+
+        if token_strength:
+            for rt in range(77) if replace_token<0 else [replace_token,]:
+                replaced = c0[:,rt,:]
+                c0[:,rt,:] = torch.mul(conditioning_nudge,token_strength) + torch.mul(replaced,1-token_strength)
+
+        if pool_strength:
+            pool:torch.Tensor = conditioning_in[0][1]['pooled_output'].clone().squeeze()
+            pool = torch.mul(conditioning_nudge[-len(pool):], pool_strength) + torch.mul(pool, 1-pool_strength)
+            dic['pooled_output'] = pool.unsqueeze(0)
+
+        ret = [[c0,dic],]
+        for more in conditioning_in[1:]: ret.append(more)
+        return( ret, )
+
+class NudgeConditioning(BaseNudge):
+    @classmethod
+    def INPUT_TYPES(cls):
+        it = {"required": { "nudge_file": ("STRING", {"default":""}) } }
+        return cls.ADD_STANDARD_INPUT_TYPES(it)
+
+    def func(self, conditioning, nudge_file:str, **kwargs):
+        c1:torch.Tensor = load_file(nudge_file)['nudge']   
+        return self._func(conditioning_in=conditioning, conditioning_nudge=c1, **kwargs)
+
+'''
+class ImageBasedNudgeConditioning(BaseNudge):
+    @classmethod
+    def INPUT_TYPES(cls):
+        it = {"required": { "clip": ("CLIP", ), "images": ("IMAGE", ) } }
+        return cls.ADD_STANDARD_INPUT_TYPES(it)
+
+    def func(self, conditioning, clip, images, **kwargs):
+        for image in images:
+            c1 = clipify(image)
+            conditioning = self._func(conditioning_in=conditioning, conditioning_nudge=c1, **kwargs)[0]
+        return (conditioning,)
+'''
